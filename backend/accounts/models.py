f9d8de80191f01a1b4b7247fc461f67d2fcaf5b5
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-09 20:54:11 +0300

    Model for users

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('driver', 'Driver'),
+        ('provider', 'Provider'),
+        ('admin', 'Admin'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='driver')
+    default_radius_km = models.DecimalField(max_digits=4, decimal_places=1, default=2.0)  # adjustable radius
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
