66844ffb73889c73a7d3e1e8bb6da01ed38a5072
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-09 22:35:51 +0300

    parking urls

---
+++ backend/parking/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    ParkingSpotListCreateView, NearbyParkingSpotsView, BookParkingSpotView,
+    NavigateToSpotView, SpotReviewCreateView, SpotAvailabilityLogListView
+)
+
+urlpatterns = [
+    path('spots/', ParkingSpotListCreateView.as_view(), name='parking_spots'),
+    path('nearby/', NearbyParkingSpotsView.as_view(), name='nearby_spots'),
+    path('book/', BookParkingSpotView.as_view(), name='book_spot'),
+    path('navigate/<int:spot_id>/', NavigateToSpotView.as_view(), name='navigate_spot'),
+    path('review/', SpotReviewCreateView.as_view(), name='spot_review'),
+    path('availability/logs/', SpotAvailabilityLogListView.as_view(), name='availability_logs'),
+]
