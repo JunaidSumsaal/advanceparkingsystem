5e4a9768d4c3773bcf3d65ac8ddfb6ae0ca0af9e
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-18 03:07:49 +0300

    hotfix backend cache

--- frontend/src/context/Auth.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { AuthContextType } from "../types/context/auth";
-
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
-);
