3b0d1c76060aae0f19b99d3fadd4a1d2d64ee7cd
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-20 12:43:30 +0300

    Refactored users types

--- frontend/src/types/context/auth.ts
+++ frontend/src/types/context/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { User } from "../User";
 
 export interface AuthContextType {
@@ -5,9 +6,22 @@ export interface AuthContextType {
   users?: User[];
   login: (token: string, refresh: string) => void;
   logout: () => void;
+  register: (
+    userData: { username: string; email: string; password: string }
+  ) => Promise<any>;
   refreshAccessToken?: () => Promise<void>;
   loading: boolean;
   error: string | null;
+  formErrors: Record<string, string[]> | null;
+  setLoading?: (loading: boolean) => void;
+  setError?: (error: string | null) => void;
+  setFormErrors?: (errors: Record<string, string[]> | null) => void;
+  setUser?: (user: User | null) => void;
+  setUsers?: (users: User[]) => void;
+  fetchUser?: () => Promise<void>;
+  isAuthenticated?: boolean;
+  totalUsers?: number;
+  totalPages?: number;
   currentPage?: number;
   setCurrentPage?: (page: number) => void;
 }
