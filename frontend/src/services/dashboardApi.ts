6aff2405f842dfbd31a14281fd9181ab4fe7f945
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-22 12:59:58 +0300

    api calls for dashboard

--- frontend/src/services/dashboardApi.ts
+++ frontend/src/services/dashboardApi.ts
@@ -1,26 +1,32 @@
-import apiHelper from '../utils/apiHelper';
-import { DASH_DRIVER, DASH_ATTENDANT, DASH_PROVIDER, DASH_SPOT_EVAL } from './constants';
+import apiHelper from "../utils/apiHelper";
+import { DASHBOARD } from "./constants";
 
 // Driver Dashboard
 export const getDriverDashboard = async () => {
-  const res = await apiHelper.get(DASH_DRIVER);
+  const res = await apiHelper.get(`${DASHBOARD}/driver/`);
   return res;
 };
 
 // Attendant Dashboard
 export const getAttendantDashboard = async () => {
-  const res = await apiHelper.get(DASH_ATTENDANT);
+  const res = await apiHelper.get(`${DASHBOARD}/attendant/`);
   return res;
 };
 
 // Provider Dashboard
 export const getProviderDashboard = async () => {
-  const res = await apiHelper.get(DASH_PROVIDER);
+  const res = await apiHelper.get(`${DASHBOARD}/provider/`);
   return res;
 };
 
 // Spot Evaluation Report
 export const getSpotEvaluationReport = async () => {
-  const res = await apiHelper.get(DASH_SPOT_EVAL);
+  const res = await apiHelper.get(`${DASHBOARD}/spot-evaluations/`);
+  return res;
+};
+
+// Admin Dashboard
+export const getAdminDashboard = async () => {
+  const res = await apiHelper.get(`${DASHBOARD}/admin/`);
   return res;
 };
