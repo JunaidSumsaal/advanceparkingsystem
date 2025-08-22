5b78d53b0ccf778109e5681958d71c02be9a5b9b
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-22 12:52:56 +0300

    hot fixes

--- frontend/src/services/budgetService.ts
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-import { BUDGETS } from './constants';
-
-export const createBudget = async (data: unknown) => {
-  const res = await api.post(`${BUDGETS}`, data);
-  return res.data;
-};
--- frontend/src/services/reportService.ts
+++
@@ -1,9 +0,0 @@
-import api from './api';
-import { REPORTS } from './constants';
-
-export const downloadReport = async (month: string, format: 'pdf' | 'csv') => {
-  const response = await api.get(`${REPORTS}/download?month=${month}&format=${format}`, {
-    responseType: 'blob',
-  });
-  return response.data;
-};
