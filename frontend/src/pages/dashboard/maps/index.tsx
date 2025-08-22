abbf871b1e0c56fbab744b9aa793f60e86a0549a
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-22 12:55:22 +0300

    maps page

---
+++ frontend/src/pages/dashboard/maps/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Text, Center} from '@chakra-ui/react';
+
+const Maps = () => {
+  return (
+    <Box as='div' className="flex-1 space-y-8 p-8 pt-6">
+      <Center>
+        <Text>
+          Coming Soon
+        </Text>
+      </Center>
+    </Box>
+  );
+};
+
+export default Maps;
