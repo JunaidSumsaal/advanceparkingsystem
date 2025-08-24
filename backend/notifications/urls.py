86fd14b3a298cae38dc90847ce8855861455fc8b
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-24 04:44:41 +0300

    fix notification issues issues

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -7,7 +7,7 @@ router.register(r'notifications', NotificationViewSet, basename='notifications')
 router.register(r'subscribe', PushSubscriptionViewSet, basename='subscribe')
 
 urlpatterns = [
-    path('notifications/mark_all_read/', NotificationViewSet.as_view({'post': 'mark_all_read'}), name='mark_all_read')
+    path('notifications/mark_all_read/', NotificationViewSet.as_view({'post': 'mark_all_read'}), name='mark_all_read'),
     path('unsubscribe/', UnsubscribeNotificationView.as_view(), name='unsubscribe'),
     path('history/', NotificationHistoryView.as_view(), name='history'),
     path('email-preference/', EmailPreferenceUpdateView.as_view(), name='email-preference'),
