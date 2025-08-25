3f8f264f7afb0ad445cc0a1bef7cb64521cd2c3d
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-08-25 04:05:01 +0300

    urls for proxy development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   server: { 
     proxy: { 
       '/api': { 
-        target: 'https://advancepackingsystem-backend.onrender.com',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       } 
